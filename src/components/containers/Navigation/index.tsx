5c644f7c6df056fafc93c20247fc1b171e0bb3a4
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-18 11:14:17 -0400

    create navigation container

--- src/components/containers/Navigation/index.tsx
+++ src/components/containers/Navigation/index.tsx
@@ -2,14 +2,24 @@ import { Button } from "../../";
 
 const navItems = ["Metrics", "Users", "Settings"];
 
+const title = "Mock Dashboard";
+
 const Navigation = () => {
   return (
-    <div className="flex flex-col w-full">
-      {navItems.map((item) => (
-        <Button styles="hover:bg-slate-300 bg-slate-200 h-[4rem] border-b-2 border-slate-50">
-          {item}
-        </Button>
-      ))}
+    <div className="w-full h-full bg-slate-600 justify-between flex flex-col">
+      <div>
+        <div className="text-center py-10 font-semibold text-xl">{title}</div>
+        <div className="flex flex-col">
+          {navItems.map((item) => (
+            <Button navigationItem onClick={() => {}}>
+              {item}
+            </Button>
+          ))}
+        </div>
+      </div>
+      <Button navigationItem onClick={() => {}}>
+        Log Out
+      </Button>
     </div>
   );
 };
--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,9 +1,21 @@
 const defaultStyles =
   "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
 
-const Button = ({ onClick, children, styles = defaultStyles }: any) => {
+const navigationItemStyles =
+  "hover:bg-slate-300 bg-slate-200 h-[4rem] border-b-2 border-slate-50 text-left px-10";
+
+type Props = {
+  onClick: () => void;
+  navigationItem?: boolean;
+  children: React.ReactNode;
+};
+
+const Button = ({ onClick, children, navigationItem = false }: Props) => {
   return (
-    <button onClick={onClick} className={styles}>
+    <button
+      onClick={onClick}
+      className={navigationItem ? navigationItemStyles : defaultStyles}
+    >
       {children}
     </button>
   );
