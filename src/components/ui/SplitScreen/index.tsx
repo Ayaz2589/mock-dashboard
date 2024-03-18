680b2f41084413d6105692895caaa339ef1cc78e
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-18 11:24:18 -0400

    update spit screen style location

--- src/components/ui/SplitScreen/index.tsx
+++ src/components/ui/SplitScreen/index.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 
-const leftClass = "flex-1 bg-slate-400";
+const defaultStyles = {
+  leftStyles: "flex-1 bg-slate-400",
+  rightStyles: "flex-1 bg-slate-500",
+};
 
-const rightClass = "flex-1 bg-slate-500";
+const dashboardStyles = {
+  leftStyles:
+    "hidden md:flex md:flex-1 bg-slate-400 min-w-[5rem] max-w-[15rem]",
+  rightStyles: "flex-1 bg-slate-500",
+};
 
 type Props = {
   children: React.ReactNode;
   leftStyles?: string;
   rightStyles?: string;
-  isNav?: boolean;
+  dashboard?: boolean;
 };
 
-const SplitScreen = ({
-  children,
-  leftStyles = leftClass,
-  rightStyles = rightClass,
-}: Props) => {
+const SplitScreen = ({ children, dashboard = false }: Props) => {
   const [left, right] = React.Children.toArray(children);
   return (
     <div className="flex h-screen bg-slate-200">
-      <div className={leftStyles}>{left}</div>
-      <div className={rightStyles}>{right}</div>
+      <div
+        className={
+          dashboard ? dashboardStyles.leftStyles : defaultStyles.leftStyles
+        }
+      >
+        {left}
+      </div>
+      <div
+        className={
+          dashboard ? dashboardStyles.rightStyles : defaultStyles.rightStyles
+        }
+      >
+        {right}
+      </div>
     </div>
   );
 };
