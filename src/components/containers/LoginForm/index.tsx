c59d1ab7bf90b5329344eaa6f43ec6440dba9278
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 16:37:26 -0400

    update form

--- src/components/containers/LoginForm/index.tsx
+++ src/components/containers/LoginForm/index.tsx
@@ -5,16 +5,17 @@ type Props = {
   handleEmailInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handlePasswordInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  email: string;
+  password: string;
 };
 
 const LoginForm = ({
   handleEmailInput,
   handlePasswordInput,
   handleSubmit,
+  email,
+  password,
 }: Props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   return (
     <Card className="sm:w-full md:w-1/2 min-w-[30rem]">
       <CardHeader>Login</CardHeader>
@@ -26,7 +27,6 @@ const LoginForm = ({
             value={email}
             onChange={(e) => {
               handleEmailInput(e);
-              setEmail(e.target.value);
             }}
             error={""}
           />
@@ -36,18 +36,12 @@ const LoginForm = ({
             value={password}
             onChange={(e) => {
               handlePasswordInput(e);
-              setPassword(e.target.value);
             }}
             error={""}
           />
           <button
             type="submit"
-            disabled={!email || !password}
-            className={`mt-4 ${
-              !email || !password
-                ? "bg-blue-300"
-                : "bg-blue-500 hover:bg-blue-700"
-            } text-white font-bold py-2 px-4 rounded`}
+            className={`mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
           >
             Submit
           </button>
