8cb41f46dddbb6062e12a6e1cf7db775cda0148b
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 16:51:00 -0400

    add auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,99 @@
+import {
+  createContext,
+  useContext,
+  useReducer,
+  ReactElement,
+  useCallback,
+} from "react";
+
+type AuthContextChildrenType = {
+  children: ReactElement;
+};
+
+type AuthStateType = {
+  email: string;
+  password: string;
+};
+
+type AuthContextType = {
+  authState: AuthStateType;
+  setEmail: (email: string) => void;
+  setPassword: (password: string) => void;
+  login: () => void;
+  logout: () => void;
+};
+
+enum AuthActionType {
+  SET_EMAIL = "SET_EMAIL",
+  SET_PASSWORD = "SET_PASSWORD",
+  LOGIN = "LOGIN",
+  LOGOUT = "LOGOUT",
+}
+
+type Actions =
+  | { type: AuthActionType.SET_EMAIL; payload: string }
+  | { type: AuthActionType.SET_PASSWORD; payload: string }
+  | { type: AuthActionType.LOGIN }
+  | { type: AuthActionType.LOGOUT };
+
+const initialAuthState: AuthStateType = {
+  email: "",
+  password: "",
+};
+
+const authReducer = (state: AuthStateType, action: Actions): AuthStateType => {
+  switch (action.type) {
+    case AuthActionType.SET_EMAIL:
+      return { ...state, email: action.payload };
+    case AuthActionType.SET_PASSWORD:
+      return { ...state, password: action.payload };
+    case AuthActionType.LOGIN:
+      // Handle login logic or state update
+      return state; // Placeholder
+    case AuthActionType.LOGOUT:
+      // Reset to initial state or handle logout logic
+      return { ...initialAuthState };
+    default:
+      return state;
+  }
+};
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }: AuthContextChildrenType) => {
+  const [state, dispatch] = useReducer(authReducer, initialAuthState);
+
+  const setEmail = useCallback((email: string) => {
+    dispatch({ type: AuthActionType.SET_EMAIL, payload: email });
+  }, []);
+
+  const setPassword = useCallback((password: string) => {
+    dispatch({ type: AuthActionType.SET_PASSWORD, payload: password });
+  }, []);
+
+  const login = useCallback(() => {
+    dispatch({ type: AuthActionType.LOGIN });
+  }, []);
+
+  const logout = useCallback(() => {
+    dispatch({ type: AuthActionType.LOGOUT });
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ authState: state, setEmail, setPassword, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
