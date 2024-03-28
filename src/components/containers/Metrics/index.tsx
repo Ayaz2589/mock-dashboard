527fb8e6d462750a33832b13d07d03359f0e20bf
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 11:25:58 -0400

    create login form

--- src/components/containers/Metrics/index.tsx
+++ src/components/containers/Metrics/index.tsx
@@ -9,49 +9,15 @@ import {
   Toggle,
   Spinner,
   Header,
+  LoginForm,
+  Login,
 } from "../../";
 
 const Metrics = () => {
-  const [value, setValue] = useState("");
-  const [error, setError] = useState("");
-
-  const options = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
-  ];
-
-  const handleSelection = (option: any) => console.log(option);
-
-  const handleOnChange = (e: any) => setValue(e.target.value);
   return (
     <div className="flex flex-col gap-5">
       <Header title="Metrics" />
-      <Card className="sm:w-full md:w-1/2 min-w-[30rem]">
-        <CardHeader>Header</CardHeader>
-        <CardContent>
-          <Input
-            placeholder="First Name"
-            onChange={handleOnChange}
-            value={value}
-            error={error}
-          />
-          <Dropdown
-            options={options}
-            handleSelection={handleSelection}
-            error={error}
-          />
-          <Toggle
-            title="test"
-            checked
-            onClick={(e) => {
-              const value = (e.target as HTMLInputElement).value;
-            }}
-          />
-          <Spinner />
-        </CardContent>
-        <CardFooter>Footer</CardFooter>
-      </Card>
+      <LoginForm />
     </div>
   );
 };
