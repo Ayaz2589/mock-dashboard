5fa138550df27446e382e6c69c21ffd03e3328d8
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 12:28:54 -0400

    create open nav button

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { OpenNavDrawerButton } from "../..";
 
-const Header = ({ title }: { title: string }) => {
-  const [isDrawerOpen, toggleDrawerOpen] = useState(false)
-  const handleButtonClick = () => {}
+type Props = {
+  title: string;
+  onNavButtonClick: () => void;
+};
+
+const Header = ({ title, onNavButtonClick }: Props) => {
+  const [isDrawerOpen, toggleDrawerOpen] = useState(false);
+  const handleButtonClick = () => {};
 
   return (
     <div className="flex w-full items-center">
       {/* The button will now only show on screens smaller than the md breakpoint */}
       <div className="md:hidden">
-        <OpenNavDrawerButton toggleDrawer={() => {}} />
+        <OpenNavDrawerButton toggleDrawer={onNavButtonClick} />
       </div>
       <h1 className="text-2xl font-semibold ml-[-3rem] md:ml-3 flex-grow text-center md:text-left">
         {title}
