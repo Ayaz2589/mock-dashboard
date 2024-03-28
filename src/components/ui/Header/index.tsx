37a152e64895da71b44032ce146f5d5d145a0cdc
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 12:11:49 -0400

    work on logic to show nav drawer

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import { OpenNavDrawerButton } from "../..";
+
 const Header = ({ title }: { title: string }) => {
+  const [isDrawerOpen, toggleDrawerOpen] = useState(false)
+  const handleButtonClick = () => {}
+
   return (
-    <div>
-      <h1 className="text-2xl font-semibold mt-[1.2rem] w-full text-center md:text-left">
+    <div className="flex w-full items-center">
+      {/* The button will now only show on screens smaller than the md breakpoint */}
+      <div className="md:hidden">
+        <OpenNavDrawerButton toggleDrawer={() => {}} />
+      </div>
+      <h1 className="text-2xl font-semibold ml-[-3rem] md:ml-3 flex-grow text-center md:text-left">
         {title}
       </h1>
     </div>
