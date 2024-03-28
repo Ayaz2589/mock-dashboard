3234e2172a1d70d495efe93bdef9e5953a9f2a6e
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 16:12:52 -0400

    update form to have a disabled button

--- src/components/containers/LoginForm/index.tsx
+++ src/components/containers/LoginForm/index.tsx
@@ -1,36 +1,53 @@
-import React from "react";
-import {
-  Input,
-  Card,
-  CardHeader,
-  CardContent,
-  CardFooter,
-  Avatar,
-} from "../..";
+import React, { useState } from "react";
+import { Input, Card, CardHeader, CardContent, CardFooter } from "../..";
 
-const LoginForm = () => {
+type Props = {
+  handleEmailInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handlePasswordInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const LoginForm = ({
+  handleEmailInput,
+  handlePasswordInput,
+  handleSubmit,
+}: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Card className="sm:w-full md:w-1/2 min-w-[30rem]">
       <CardHeader>Login</CardHeader>
       <CardContent>
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleSubmit}>
           <Input
             type="text"
             placeholder="Email"
-            value={""}
-            onChange={() => {}}
+            value={email}
+            onChange={(e) => {
+              handleEmailInput(e);
+              setEmail(e.target.value);
+            }}
             error={""}
           />
           <Input
             type="password"
             placeholder="Password"
-            value={""}
-            onChange={() => {}}
+            value={password}
+            onChange={(e) => {
+              handlePasswordInput(e);
+              setPassword(e.target.value);
+            }}
             error={""}
           />
           <button
             type="submit"
-            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            disabled={!email || !password}
+            className={`mt-4 ${
+              !email || !password
+                ? "bg-blue-300"
+                : "bg-blue-500 hover:bg-blue-700"
+            } text-white font-bold py-2 px-4 rounded`}
           >
             Submit
           </button>
