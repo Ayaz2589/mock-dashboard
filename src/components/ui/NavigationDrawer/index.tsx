bc456a1dadd7a72a2a4d4bdd393ca0ed1ae0f7fa
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-28 14:30:47 -0400

    make drawer full width

--- src/components/ui/NavigationDrawer/index.tsx
+++ src/components/ui/NavigationDrawer/index.tsx
@@ -1,33 +1,16 @@
-import React, { useEffect } from "react";
 import { Navigation } from "../..";
 import { useNavigation } from "../../../hooks";
 
 const DrawerButton = () => {
   const { navigationState, toggleDrawer } = useNavigation();
 
-  useEffect(() => {
-    if (navigationState.isDrawerOpen) {
-      // Prevent scrolling on body
-      document.body.style.overflow = "hidden";
-    } else {
-      // Re-enable scrolling on body
-      document.body.style.overflow = "";
-    }
-
-    // Cleanup function to re-enable scrolling when component unmounts
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, [navigationState.isDrawerOpen]); // Dependency array ensures effect runs only when isDrawerOpen changes
-
   return (
     <div>
       <div
-        className={`fixed top-0 left-0 w-64 h-full bg-slate-700 transform ${
+        className={`fixed top-0 left-0 w-full h-full bg-slate-700 transform ${
           navigationState.isDrawerOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out z-50 md:hidden`}
       >
-        {/* Close Button */}
         <button
           onClick={toggleDrawer}
           className="absolute top-0 right-0 p-2 m-2 text-white"
