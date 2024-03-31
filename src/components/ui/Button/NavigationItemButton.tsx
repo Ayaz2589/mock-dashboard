f09a9c54c0b0ed4fc449c6cbb88c05d1dd645cd7
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-31 13:10:45 -0400

    add clicked state to nav

--- src/components/ui/Button/NavigationItemButton.tsx
+++ src/components/ui/Button/NavigationItemButton.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
+import { useLocation } from "react-router-dom";
 
 type Props = {
   onClick?: () => void;
   children: React.ReactNode;
+  link: string;
 };
 
-const NavigationItemButton = ({ children, onClick }: Props) => {
+const NavigationItemButton = ({ children, onClick, link }: Props) => {
+  const location = useLocation();
+
+  const baseClass =
+    "h-[4rem] text-left px-10 text-slate-300 hover:bg-primary-hover";
+  const activeClass = "bg-primary-hover";
+  const inactiveClass = "bg-primary";
+
+  const className = twMerge(
+    baseClass,
+    location.pathname === link ? activeClass : inactiveClass
+  );
+
+  console.log(location.pathname);
   return (
-    <button
-      onClick={onClick}
-      className="hover:bg-primary-hover bg-primary h-[4rem] text-left px-10 text-slate-300"
-    >
+    <button onClick={onClick} className={className}>
       {children}
     </button>
   );
