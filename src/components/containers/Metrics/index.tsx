cd8d10cbbca870d4d966dd4e0ca1ba99867f9149
Ayaz Uddin <ayazuddin@ayazs-mbp.mynetworksettings.com>
2024-03-31 12:40:29 -0400

    remove contents from pages

--- src/components/containers/Metrics/index.tsx
+++ src/components/containers/Metrics/index.tsx
@@ -1,30 +1,9 @@
-import { useState } from "react";
-import { Header, Dropdown } from "../../";
-import { useAuth } from "../../../hooks";
+import { Header } from "../../";
 
 const Metrics = () => {
-  const { authState } = useAuth();
-  console.log(authState);
-
-  const [value, setValue] = useState("");
-  const [error, setError] = useState("");
-
-  const options = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
-  ];
-
-  const handleSelection = (option: any) => console.log(option);
-  
   return (
     <div className="flex flex-col gap-5">
       <Header title="Metrics" />
-      <Dropdown
-        options={options}
-        handleSelection={handleSelection}
-        error={error}
-      />
     </div>
   );
 };
